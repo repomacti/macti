6d464ee09ee87ff7868d0f763271839d2cc070bd
luiggix <luiggix@gmail.com>
2024-11-30 01:17:32 +0000

    plot vectors improved

--- notebooks/Algebra_Lineal_01/vecspace.py
+++ notebooks/Algebra_Lineal_01/vecspace.py
@@ -1,18 +1,16 @@
 import numpy as np
 import math
 import ipywidgets as wd
-import macti.visual as mvis
+#import macti.visual as mvis
+import visual as mvis
 
-def saxpy(α, x, y):
-    αxpy = α * x + y
+def saxpy(α, x, y, w):
+#    αxpy = α * x + y
+    print('\n α * x = {} \t α * x + y = {}\n'.format(α * x, α * x + y))
 
-    print(' α = {} \t x = {} \t y = {}'.format(α, x, y))
-    print(' α * x = {} '.format(α * x))
-    print(' α * x + y = {} \n'.format(α * x + y))
-
-    v = mvis.Plotter(fig_par = dict(figsize=(5,5)))
+    v = mvis.Plotter(1, 1, [dict(aspect='equal')], fig_par = dict(figsize=(5,5)))
     v.set_coordsys()
-    v.plot_vectors_sum(1, [α*x, y], ['α*x', 'y'])
+    v.plot_vectors_sum(1, [α * x, y], ['α*x', 'y'], w = [w, w, w])
     v.grid()
     v.show()
 
@@ -46,17 +44,33 @@ w_a = wd.FloatSlider(
     layout=wd.Layout(width='250px')
 )
 
+w_w = wd.FloatSlider(
+    min=0.01, max=0.05, step=0.01, value=0.01,
+    description='w',
+    layout=wd.Layout(width='250px')
+)
+
 button = wd.Button(
     description="", icon='play',
     layout=wd.Layout(width='50px')    
 )
 
-ui = wd.HBox([wd.VBox([w_a, button]),
-              wd.VBox([w_x1, w_y1]),
-              wd.VBox([w_x2, w_y2])])
+ui = wd.VBox([wd.HBox([w_a, w_w],
+                      layout=wd.Layout(padding='0px', 
+                                       border='1px solid gray',
+                                       width='510px')),
+              wd.HBox([wd.VBox([w_x1, w_x2],
+                              layout=wd.Layout(padding='0px', 
+                                               border='1px solid gray',
+                                               width='255px')),
+                       wd.VBox([w_y1, w_y2],
+                              layout=wd.Layout(padding='0px', 
+                                               border='1px solid gray',
+                                               width='255px')),                      
+                      ]),
+              wd.HBox([button])])
 
-ui.layout = wd.Layout(border='solid 1px black')
-ui.layout.width = '900px'
+ui.layout = wd.Layout(border='solid 0px black')
             
 output = wd.Output()
 
@@ -66,6 +80,7 @@ def on_button_clicked(b):
     output.clear_output(wait=True)
     with output:
         saxpy(w_a.value, np.array([w_x1.value, w_x2.value]), 
-                         np.array([w_y1.value, w_y2.value]))
+                         np.array([w_y1.value, w_y2.value]),
+              w_w.value)
 
 button.on_click(on_button_clicked)
